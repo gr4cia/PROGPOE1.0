f4357f2e72403d7b6d99b62b22e2294de2c8b493
Student <Student@LabVM2049939.4vfycjoyuesejaxnlahlnrr3ge.jnbx.internal.cloudapp.net>
2026-09-11 17:25:54 +0200

    second commit

---
+++ src/test/java/com/mycompany/poe/PoeTest.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.poe;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Student
+ */
+public class PoeTest {
+    
+    public PoeTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        
+    }
+    
+}

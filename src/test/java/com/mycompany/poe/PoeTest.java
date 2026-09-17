1e8a08d10f79b5ad78e440fcf4f27f2f1399943f
Student <Student@LabVM2049939.4vfycjoyuesejaxnlahlnrr3ge.jnbx.internal.cloudapp.net>
2026-09-17 23:05:38 +0200

    third commit

--- src/test/java/com/mycompany/poe/PoeTest.java
+++ src/test/java/com/mycompany/poe/PoeTest.java
@@ -1,4 +1,12 @@
-package com.mycompany.poe;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */package com.mycompany.poe;
+
+/**
+ *
+ * @author Student
+ */
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
